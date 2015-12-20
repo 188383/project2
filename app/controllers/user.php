a846524d0b1ef19865594a016eb18ea838f5f589
188383 <188383@student.pwr.edu.pl>
2015-12-20 23:39:21 +0100

    A completely different approach, looking more restful

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -1,138 +1,132 @@
 <?php
-/*
-	This is the user model wrapped in fatfree's data mapper.
-	This extended functionality allows us to use our own functions
-	
-	db - global constant in config.ini
-*/
-
-
-
 class User extends Controller{
-
-	protected $user;	
-	protected $room;
-	protected $audit;
+	protected $user;
 	protected $bcrypt;
+	protected $audit;
 	function __construct(){
 		parent::__construct();
-		$this->user = new Users($this->db);
-		$this->room = new Rooms($this->db);
-		$this->auth = \Audit::instance();
+		$this->user = new \DB\SQL\Mapper($this->db,'users');
+		$this->audit = \Audit::instance();
 		$this->bcrypt = \BCrypt::instance();
 	}
 	
-	/**
-		Function that processes the user who registers for the website.
-		The user email is validated as an email to check for correctness of format.
-		If the email is incorrect, the user is then requested to enter again with correct credentials
-		
-		TODO Add a user security question so that a user can easily reset their password
-	**/
 	public function create($f3){
-		$this->user = new Users($this->db);
-		//extract the variables submitted by the POST method
-		$userdetails = $f3->get('POST');
-		$username = $userdetails['username'];
-		$password = $userdetails['password'];
-		$firstname = $userdetails['firstname'];
-		$lastname = $userdetails['lastname'];	
-		
-		$this->user->reset();
-		$this->user->load(array('username=:username','username=>' . $username));
-		
+		$post = $f3->get('POST');
+		$message = array();
 		try{
-			if(!$this->auth->email($username)) throw new Exception('Not an email');
-			if(!$this->user->dry()) throw new Exception('User exists'); 
-			
-			$this->user->firstname = $firstname;
-			$this->user->lastname = $lastname;
-			$this->user->username = $username;
-			$this->user->password = $this->hashPassword($password);
-			$this->user->save();
-		}catch(Exception $e){
-			$f3->set('message',$e);
-			$f3->reroute('/register');
-			//Handle the error and reroute the user appropriately
-		}
-		
-	}
-	
-	
-	
-	/**
-		Destroy the session data
-	**/
-	
-
-	
-	
-	public function rooms($f3){
-		$this->room->reset();
-		$this->room->load(array('ownerid=?', $f3->get('SESSION.userid')));
-		
-		if($this->room->dry()){
-			echo 'dry';
-		}else{
-			$users = array();
-			$users[$this->room->id] = $this->room->id;
-			while($this->user->skip()){
-				
-			} 
-		}
-	}
-	
-	//generate the add room formula
-	public function addRoom($f3){
-	
-		//echo Template::instance()->render('newroom.htm');
-	}
-	
-	public function listUsers(){
-		$username = null;
-		$firstname = null;
-		$lastname = null;
-		return $this->user->find(array(),array('order'=>'username','limit'=>1));
-	}	
-	
-		public function authenticate($f3){
-		$userdetails = $f3->get('POST');
-		
-		$username = $userdetails['username'];
-		$password = $userdetails['password'];
-		
-		/*if($username===$f3->get('SESSION.username')){
-			$f3->reroute($f3->get('dashboardurl'));
-		}*/
-		
-		try{
-			if(!$this->auth->email($username)) throw new Exception('User and or password is not correct');
-			
 			$this->user->reset();
-			$this->user->load(array('username=?',$username));
-			echo $this->user->firstname;
-			if(!$this->bcrypt->verify($password,$this->user->password)) throw new Exception('password');
+			$this->user->username = $this->validateEmail($post['username']);
+			$this->user->password = $this->hashPassword($post['password']);
+			$this->user->firstname = $post['firstname'];
+			$this->user->lastname = $post['lastname'];
+			$this->user->save();
 			
-			$f3->set('SESSION.username',$this->user->username);
-			$f3->set('SESSION.userid',$this->user->id);
-			$f3->reroute('/user?id=' . $f3->get('SESSION.userid') );
+			$message['id'] = $this->user->id;
+			$message['username'] = $this->user->username;
+			$message['password'] = $this->user->password;
+			$message['firstname'] = $this->user->firstname;
+			$message['lastname'] = $this->user->lastname;
+			
 		}catch(Exception $e){
-			echo $e;
-			//handle appropriate errors
+			$message['success'] = false;
+			$message['error'] = $e->getMessage();
 		}
 		
-	}	
+		echo json_encode($message);
+	}
 	
-	private function emailValidate($email){
-		return $this->auth->email($email);
-	}	
-
-	private function hashPassword($password){
+	public function findById($f3,$params){
+		$message = array();
+		
+		try{	
+			$this->user->reset();
+			$this->user->load(array('id=?',$params['username']));
+			if($this->user->dry()) throw new Exception('user error');
+			$array = array();
+			$array['username'] = $this->user->username;
+			$array['firstname'] = $this->user->firstname;
+			$array['lastname'] = $this->user->lastname;
+			$array['id'] =  $this->user->id;	
+			$message['success'] = true;
+			$message['data'] = $array;
+		}catch(Exception $e){
+			$message['success'] = false;
+			$message['error'] = $e->getMessage();
+		}
+			
+		echo json_encode($message);
+	}
+	
+	public function update($f3,$params){
+		$message = array();
+		$data = array();
+		try{
+			$this->user->reset();
+			$this->user->load(array('id=?',$params['username']));
+			$this->user->firstname = $f3->get('POST.firstname');
+			$this->user->lastname = $f3->get('POST.lastname');
+			$this->user->update();
+			
+			$data['firstname'] = $this->user->firstname;
+			$data['lastname'] = $this->user->lastname;
+			$data['username'] = $this->user->username;
+			$message['success'] = true;
+			$message['data'] = $data;
+			
+			
+		}catch(Exception $e){
+			$message['success'] = false;
+			$message['error'] = 'update error!';
+		}
+		
+		echo json_encode($message);
+	}
+	
+	public function findUsers($f3){
+		$get = $f3->get('GET');
+		$message = array();
+		$limit = 10;
+		$data = array();
+		$this->user->load(array());
+		
+		if(isset($get['skip'])){
+			$this->user->skip($get['skip']);
+		}
+		
+		if(isset($get['limit'])){
+			$limit = $get['limit'];
+		}
+		
+		try{
+		
+			while($limit>=0){
+				$data['id'] = $this->user->id;
+				$data['username'] = $this->user->username;
+				$data['firstname'] = $this->user->firstname;
+				$data['lastname'] = $this->user->lastname;
+				$message[count($message)] = $data;
+				$limit = $limit - 1;
+			}
+			
+		}catch(Exception $e){
+			$message['success'] = false;
+			$message['error'] = 'data error';
+		}
+		
+		echo json_encode($message);
+	}
+	
+	public function validateEmail($email){
+		if(!$this->audit->email($email)) throw new Exception('email');
+		return $email;
+	}
+	
+	public function hashPassword($password){
 		return $this->bcrypt->hash($password);
 	}
 	
-	function beforeRoute($f3){
-		parent::beforeRoute($f3);
-	}	
-	
+	public function checkName($name){
+		if(!preg_match('/[a-z]/i',$name)) throw new Exception('name or surname');
+	}
 }
+?>
